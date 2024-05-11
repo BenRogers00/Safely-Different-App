c6e2ea24527177646f89c65eee08411c802dae69
BenRogers00 <benr004@outlook.com>
2024-05-12 02:02:53 +1200

    Updated sign up/in to hide if signed in

--- safely-different/src/App.js
+++ safely-different/src/App.js
@@ -1,15 +1,12 @@
 // This is the App.js file that will be used to render different components.
 
 import React from 'react';
-import SignUp from './components/auth/SignUp';
-import SignIn from './components/auth/SignIn';
 import AuthDetails from './components/AuthDetails';
 
 function App() {
   return (
     <div className="App">
-        <SignUp />
-        <SignIn />
+      {/*note that I have reworked SignIn and SignUp slightly, so it is called only if the user is signed in */}
         <AuthDetails />
     </div>
   );
