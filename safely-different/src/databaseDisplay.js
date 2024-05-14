79820ebb2c710a6ca7c96caa89a8c7bab1656211
BenRogers00 <benr004@outlook.com>
2024-05-14 16:04:30 +1200

    Removed some firebase structure that wasn't needed, and utilised Elena's firebase.js file

--- safely-different/src/databaseDisplay.js
+++ safely-different/src/databaseDisplay.js
@@ -1,27 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, onValue } from 'firebase/database';
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAdE_nE9Nk2RHU07QVdE582mqpip59Mgx4",
-  authDomain: "safely-different.firebaseapp.com",
-  projectId: "safely-different",
-  storageBucket: "safely-different.appspot.com",
-  messagingSenderId: "1039652889761",
-  appId: "1:1039652889761:web:340c3733cef4bd24343ce0",
-  measurementId: "G-ER3JKS4E2K",
-  //added database URL for the location error
-  databaseURL: "https://safely-different-default-rtdb.asia-southeast1.firebasedatabase.app"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
-const database =getDatabase();
+import { database } from './firebase/firebase';
+import { ref, onValue } from 'firebase/database';
 
 
 function DisplayDatabase() {
