264ae92c4884f73e2303c48d288175815d9f4d28
BenRogers00 <benr004@outlook.com>
2024-05-14 16:04:30 +1200

    Removed some firebase structure that wasn't needed, and utilised Elena's firebase.js file

--- safely-different/src/checkValueInDB.js
+++ safely-different/src/checkValueInDB.js
@@ -3,27 +3,10 @@
 
 import './App.css';
 import { useEffect } from 'react';
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue } from 'firebase/database';
+import { ref, onValue } from 'firebase/database';
 import { useState } from 'react';
+import { database } from './firebase/firebase';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyAdE_nE9Nk2RHU07QVdE582mqpip59Mgx4",
-    authDomain: "safely-different.firebaseapp.com",
-    projectId: "safely-different",
-    storageBucket: "safely-different.appspot.com",
-    messagingSenderId: "1039652889761",
-    appId: "1:1039652889761:web:340c3733cef4bd24343ce0",
-    measurementId: "G-ER3JKS4E2K",
-    //added database URL for the location error
-    databaseURL: "https://safely-different-default-rtdb.asia-southeast1.firebasedatabase.app"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase();
 
 //function will take in a string for a path, and an expected result to compare to
 function useCheckValueInDB(path, expectedResult) {
