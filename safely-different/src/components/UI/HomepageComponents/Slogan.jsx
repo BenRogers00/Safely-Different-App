de47bf479c895bdc5954c01ca4c016dc61e7fe26
giangh00 <162215746+giangh00@users.noreply.github.com>
2024-05-20 02:17:02 +1200

    dark/white theme

--- safely-different/src/components/UI/HomepageComponents/Slogan.jsx
+++ safely-different/src/components/UI/HomepageComponents/Slogan.jsx
@@ -4,13 +4,13 @@ function Slogan(){
         <>
          <section className=" my-14 font-extrabold">
              <div className="text-center">
-                <p className="text-32 text-purple-300">
+                <p className="text-32 dark:text-purple-300 text-black">
                 First Step Solution
                 </p>
-                <p className="text-26  text-gray-300">
+                <p className="text-26  text-gray-500">
                 Your partner for impactful delivery, custom solutions, and success. 
                 </p>
-                <button className="font-serif text-20 mt-3 py-1 px-4 font-extrabold bg-purple-500 rounded-[14px] hover:bg-violet-400">Join us</button>
+                <button className="font-serif text-20 mt-3 py-1 px-4 font-extrabold dark:bg-purple-500 rounded-[14px] dark:hover:bg-violet-400 bg-green-100 text-black">Join us</button>
              </div>
  
         <div>
---
+++ safely-different/src/components/UI/HomepageComponents/useDarkMode.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+function useDarkMode(){
+
+    const [isDarkMode, setDarkMode] = useState(() => {
+        const savedTheme = localStorage.getItem("theme");
+        return savedTheme ? savedTheme === 'dark' : false;
+    });
+
+    const toggleDarkMode = () => {
+        setDarkMode(prevMode => !prevMode);
+    };
+
+    useEffect(() => {
+        const html = window.document.documentElement;
+        const currentTheme = isDarkMode ? "dark" : "light";
+        const previousTheme = isDarkMode ? "light" : "dark";
+        
+        html.classList.remove(previousTheme);
+        html.classList.add(currentTheme);
+        
+        localStorage.setItem("theme", currentTheme);
+    }, [isDarkMode]);
+
+    return [isDarkMode, toggleDarkMode];
+}
+
+export default useDarkMode;
