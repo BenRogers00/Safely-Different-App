b032f4ba4bf6340672a8e69bcb01099b1ce0b92d
BenRogers00 <benr004@outlook.com>
2024-05-20 16:11:44 +1200

    updated so database isn't overwritten

--- safely-different/src/renderOneEntry.js
+++ safely-different/src/renderOneEntry.js
@@ -13,6 +13,8 @@ function ReadOneDB(path) {
         onValue(databaseRef, (snapshot) => {
             //make a const for ease of use and reading
             const data = snapshot.val();
+            console.log(path)
+            console.log(data)
             //update the databaseContent variable
             setDatabaseContent(data);
         });
