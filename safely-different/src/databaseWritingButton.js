ae50b6e675eb8a8f4c1d9a844f5ed1d72cfca11c
BenRogers00 <benr004@outlook.com>
2024-05-21 11:16:19 +1200

    minor bug fix, removed manual push to db button

--- safely-different/src/databaseWritingButton.js
+++ safely-different/src/databaseWritingButton.js
@@ -3,7 +3,7 @@ import WriteToDatabase from './databaseWriting';
 function WriteButton({value}) {
     //functionality for button, just gets the inputted value, and calls WriteToDatabase with the correct path
     const handleClick = () => {
-        WriteToDatabase({dataInput: { value }}, 'writingTest/textFieldTest1')
+        WriteToDatabase({dataInput: { value }}, '/textFieldTest1')
     };
 
     return (
