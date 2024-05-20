d5fb6ce5ed5af929457f336d90d50ad4b4337208
Elena Fedchenko <fedchenko.yelena@gmail.com>
2024-05-20 16:46:04 +1200

    started merging front and back

--- safely-different/src/App.js
+++ safely-different/src/App.js
@@ -1,15 +1,19 @@
 // This is the App.js file that will be used to render different components.
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePageUI from './components/UI/HomepageComponents/HomePageUI';
 import React from 'react';
+import SignIn from './components/auth/SignIn';
 import AuthDetails from './components/AuthDetails';
 
 function App() {
   return (
     <div className="App">
-      {/*note that I have reworked SignIn and SignUp slightly, so it is called only if the user is signed in */}
-       {/*<AuthDetails/>*/} 
-       <HomePageUI/>
-      
+       <Router>
+          <Routes>
+            <Route path="/" element={<HomePageUI />} />
+            <Route path="/signin" element={<SignIn />} />
+            </Routes>
+        </Router>
         
     </div>
   );
