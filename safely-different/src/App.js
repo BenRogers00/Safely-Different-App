dfc47a5ed3f6377e05ee49d6e614790cf2d35d8e
Elena Fedchenko <fedchenko.yelena@gmail.com>
2024-05-21 20:41:13 +1200

    modified authDetails so it saves the status of the user correctly and logs out

--- safely-different/src/App.js
+++ safely-different/src/App.js
@@ -1,12 +1,9 @@
 // This is the App.js file that will be used to render different components.
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePageUI from './components/UI/HomepageComponents/HomePageUI';
 import React from 'react';
-// import SignIn from './components/auth/SignIn';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
-// import SignUp from './components/UI/HomepageComponents/SignUp';
-// import SignIn from './components/UI/HomepageComponents/SingIn';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import AuthDetails from './components/AuthDetails';
 
@@ -14,13 +11,15 @@ function App() {
   return (
     <div className="App">
        <Router>
+        <AuthDetails>
           <Routes>
             <Route path="/" element={<HomePageUI />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
 
             </Routes>
-        </Router>
+        </AuthDetails>
+      </Router>
         
     </div>
   );
