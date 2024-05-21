750a2aca93a8862ec99d8b4942128f68fbe58898
Elena Fedchenko <fedchenko.yelena@gmail.com>
2024-05-21 20:41:13 +1200

    modified authDetails so it saves the status of the user correctly and logs out

--- safely-different/src/components/UI/HomepageComponents/NavBar.jsx
+++ safely-different/src/components/UI/HomepageComponents/NavBar.jsx
@@ -1,154 +1,92 @@
-// import { useState } from "react";
-// import { BsSun } from "react-icons/bs";
-// import { HiOutlineMenu } from "react-icons/hi";
-// import { MdClose } from "react-icons/md";
-// import { FaMoon } from "react-icons/fa";
-// import { Link } from 'react-router-dom';
-// // import { useAuth } from "../../AuthDetails";
-// import useDarkMode from "./useDarkMode";
-// import AuthDetails from "../../AuthDetails";
 
-
-// function NavBar(props) {
-//  // const {Mobile} = props;
-//   const [openMenu, setOpenMenu] = useState(false);
-//   const [isDarkMode, toggleDarkMode] = useDarkMode();
-//   // const { currentUser } = useAuth();
-//   const handleMenu = () => {
-//     setOpenMenu(!openMenu);
-//   };
-
-//   return (
-//     <>
-      
-//         <nav className="  flex items-center ">
-//           <div className="flex items-center">
-//             <div className="mr-2">
-//               <a href=" ">
-//                 <img
-//                   src="/images/logotype_dark.PNG"
-//                   alt="null"
-//                   className="md:w-[150px] w-[130px]"
-//                 />
-//               </a>
-//             </div>
-//             {isDarkMode?
-//             (<BsSun size={"30px"} color="#e9c46a" className="cursor-pointer"  onClick={()=>toggleDarkMode()}/>):
-//             (<FaMoon size={"30px"} color="#e9c46a" className="cursor-pointer"  onClick={()=>toggleDarkMode()}/>)}
-             
-            
-//           </div>
-      
-//           <ul className="ml-auto ">            {/* Open menu from set state, if Mobile is from props, if it is mobile and openmueny, close icon is displayed */}
-//             {openMenu && props.Mobile ? (             
-//               <MdClose
-//                 size={"30px"}
-//                 color="grey"
-//                 className="cursor-pointer"
-//                 onClick={handleMenu}
-//               />
-//             ) : !openMenu && props.Mobile ?(
-//               <HiOutlineMenu
-//                 size={"30px"}
-//                 color="gray"
-//                 className="cursor-pointer"
-//                 onClick={handleMenu}
-//               />
-//             ):(
-//               <>
-//               <div className="flex text-[20px] text-black dark:text-slate-100">
-//               <li className="mx-4 hover:text-purple-500 transition-all font-mono cursor-pointer">Home</li>
-//               <li className="mx-4 hover:text-purple-500 transition-all font-mono cursor-pointer">Price</li>
-//               <li className="mx-4 hover:text-purple-500 transition-all font-mono cursor-pointer">Our-Team</li>
-//               <Link to="../auth/SignIn">Sign-In</Link>
-
-//               {/* <li className="mx-20 hover:text-purple-500 transition-all font-mono cursor-pointer">Sign-In</li> */}
-//               </div>
-              
-//               </>
-//             )}
-// {/* if openmenu open and required, then display the bottom  */}
-//             {openMenu && (
-//               <div className="fixed right-2 bg-slate-100  p-8 text-center text-15 text-black">
-//                 <li className="hover:text-purple-500 transition-all cursor-pointer">Home</li>
-//                 <li className="hover:text-purple-500 transition-all cursor-pointer">Price</li>
-//                 <li className="hover:text-purple-500 transition-all cursor-pointer">Our Team</li>
-//                 <Link to="../auth/SignIn">Sign-In</Link>
-//               </div>
-//             )}
-//           </ul>
-//         </nav>
-//         <AuthDetails/>
-      
-//     </>
-//   );
-// }
-
-// export default NavBar;
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { BsSun } from "react-icons/bs";
 import { HiOutlineMenu } from "react-icons/hi";
 import { MdClose } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { AuthContext } from '../../AuthDetails';
 
 function NavBar(props) {
   const {isMobile} = props;
   const [openMenu, setOpenMenu] = useState(false);
+  const { authUser, userSignOut } = useContext(AuthContext); 
+  
+  // Set the openMenu state to the opposite of its current value
   const handleMenu = () => {
     setOpenMenu(!openMenu);
   };
 
+  // Function to handle user sign out
+  const handleSignOut = () => {
+    userSignOut().then(() => {
+      console.log("Logged out successfully");
+      // Additional logout handling if needed
+    }).catch((error) => {
+      console.error("Logout error", error);
+    });
+  };
+
   return (
     <>
-      
-        <nav className="  flex items-center ">
-          <div className="flex items-center">
-            <div className="mr-2">
-              <a href=" ">
-                <img
-                  src="/images/logotype_dark.PNG"
-                  alt="null"
-                  style={{ width: "100px", height: "auto" }}
-                />
-              </a>
-            </div>
-            <BsSun size={"30px"} color="#e9c46a" className="cursor-pointer" />
+      <nav className="  flex items-center ">
+        <div className="flex items-center">
+          <div className="mr-2">
+            <a href=" ">
+              <img
+                src="/images/logotype_dark.PNG"
+                alt="null"
+                style={{ width: "100px", height: "auto" }}
+              />
+            </a>
           </div>
-      
-          <ul className="ml-auto gap-4">
-            {openMenu && isMobile ? (
-              <MdClose
-                size={"30px"}
-                color="grey"
-                className="cursor-pointer"
-                onClick={handleMenu}
-              />
-            ) : !openMenu && isMobile ?(
-              <HiOutlineMenu
-                size={"30px"}
-                color="grey"
-                className="cursor-pointer"
-                onClick={handleMenu}
-              />
-            ):(
-              <>
-              <div className="flex ">
+          <BsSun size={"30px"} color="#e9c46a" className="cursor-pointer" />
+        </div>
+    
+        <ul className="ml-auto gap-4">
+          {openMenu && isMobile ? (
+            <MdClose
+              size={"30px"}
+              color="grey"
+              className="cursor-pointer"
+              onClick={handleMenu}
+            />
+          ) : !openMenu && isMobile ?(
+            <HiOutlineMenu
+              size={"30px"}
+              color="grey"
+              className="cursor-pointer"
+              onClick={handleMenu}
+            />
+          ):(
+            <>
+            <div className="flex ">
               <li className="mx-2">
                 <Link to="/">Home</Link></li>
               <li className="mx-2">
                 <Link to="/price">Price</Link></li>
               <li className="mx-2">
                 <Link to="/about">About</Link></li>
-              <li className="mx-2">
-                <Link to="/SignIn">Sign-In</Link></li>
-              <li className="mx-3">
-                <Link to="/SignUp">Sign-Up</Link></li>
-              </div>
-              
-              </>
-            )}
+              {authUser ? (
+                <>
+                  <li className="mx-2">{authUser.email}</li>
+                  <li className="mx-2">
+                    <Link to="/" onClick={handleSignOut}>Log Out</Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                <li className="mx-2">
+                  <Link to="/SignIn">Sign-In</Link></li>
+                <li className="mx-3">
+                  <Link to="/SignUp">Sign-Up</Link></li>
+                </>
+              )}
+            </div>
+            
+            </>
+          )}
 
-{openMenu && (
+          {openMenu && (
             <div className="fixed right-2 bg-slate-100 p-8 text-center text-13 text-black">
               <li>
                 <Link to="/" onClick={handleMenu}>Home</Link>
@@ -159,12 +97,24 @@ function NavBar(props) {
               <li>
                 <Link to="/about" onClick={handleMenu}>About</Link>
               </li>
-              <li>
-                <Link to="/sign-in" onClick={handleMenu}>Sign-In</Link>
-              </li>
-              <li>
-                <Link to="/sign-up" onClick={handleMenu}>Sign-Up</Link>
-              </li>
+              {authUser ? (
+                  <>
+                    <li className="mx-2">{authUser.email}</li>
+                    <li className="mx-2">
+                      <Link to="/" onClick={handleSignOut}>Log Out</Link>
+                    </li>
+                  </>
+                ) : (
+                  <>
+                  <li className="mx-2">
+                    <Link to="/sign-in" onClick={handleMenu}>Sign-In</Link>
+                    </li>
+                  <li className="mx-3">
+                    <Link to="/sign-up" onClick={handleMenu}>Sign-Up</Link>
+                    </li>
+
+                  </>
+                )}
             </div>
           )}
         </ul>
@@ -174,3 +124,4 @@ function NavBar(props) {
 }
 
 export default NavBar;
+
--- safely-different/src/components/auth/SignIn.jsx
+++ safely-different/src/components/auth/SignIn.jsx
@@ -1,8 +1,8 @@
 // This component is used to sign in a user using their email and password.
 
 import React, { useState } from 'react';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../../firebase/firebase';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
 
