a3f8114761d50a857258f0fcd327d8a8f7e41f0b
Elena Fedchenko <fedchenko.yelena@gmail.com>
2024-06-10 20:08:13 +1200

    added babel

--- safely-different/jest.config.js
+++ safely-different/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    transform: {
+      '^.+\\.(js|jsx)$': 'babel-jest',
+    },
     moduleNameMapper: {
       '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.js',
     },
