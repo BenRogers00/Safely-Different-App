916fa52feac4b240df74671382ebab4f39ba2b77
Elena Fedchenko <fedchenko.yelena@gmail.com>
2024-07-23 10:01:49 +1200

    Added a payment mockup which updates the privilege to paid

--- safely-different/src/components/UI/HomepageComponents/PricePanel.jsx
+++ safely-different/src/components/UI/HomepageComponents/PricePanel.jsx
@@ -42,7 +42,7 @@ function PricePanel(){
                   <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
                 </p>
                 <a
-                  href="#"
+                  href="/memberPricing"
                   className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Get access
