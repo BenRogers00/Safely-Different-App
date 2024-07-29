273a08b730d7055307d77efe57b17b58879deec3
BenRogers00 <benr004@outlook.com>
2024-07-30 11:53:02 +1200

    add some CSS to posts (temporary W.I.P)

--- safely-different/src/components/blogPosts/blogPostDisplay.jsx
+++ safely-different/src/components/blogPosts/blogPostDisplay.jsx
@@ -7,7 +7,7 @@ import { readOneDBCallback } from '../../readOneEntry';
 import CommentDisplay from './readAllComments';
 import DrawingComponent from '../drawing/DrawingComponent';
 import NavBar from '../UI/HomepageComponents/NavBar';
-
+import './blogDisplay.css'; 
 
 function BlogDisplay() {
     const [posts, setPosts] = useState([]);
@@ -27,12 +27,12 @@ function BlogDisplay() {
         };
     }, []);
 
-    //get user's email from their post
+    // Get user's email from their post
     useEffect(() => {
         const fetchUserEmails = () => {
             const emails = {};
             posts.forEach(post => {
-                //use callback function to get a user's email to use later
+                // Use callback function to get a user's email to use later
                 readOneDBCallback(`users/${post.user}/email`, (email) => {
                     emails[post.user] = email;
                     setUserEmails(prevEmails => ({ ...prevEmails, [post.user]: email }));
@@ -40,21 +40,21 @@ function BlogDisplay() {
             });
         };
 
-        //if there are posts, get the emails from the posts
+        // If there are posts, get the emails from the posts
         if (posts.length > 0) {
             fetchUserEmails();
         }
     }, [posts]);
 
-    //get the path to use when getting info from the database
+    // Get the path to use when getting info from the database
     function getStrippedPath(inputString) {
         const parts = inputString.split('/');
-        parts.splice(0, 3); // remove first 3 parts
+        parts.splice(0, 3); // Remove first 3 parts
         const path = parts.join('/');
         return path;
     }
 
-    //function to show/hide the comment text box
+    // Function to show/hide the comment text box
     function toggleCommentBox(key) {
         setOpenCommentBoxes(prevState => ({
             ...prevState,
@@ -62,39 +62,37 @@ function BlogDisplay() {
         }));
     }
 
-    //display for blog posts
+    // Display for blog posts
     return (
+        <div>
+            <NavBar Mobile={false} /> {/* putting props inside mobile */}
+
         <div id="blogDisp">
-            <NavBar Mobile ={false}/> {/* putting props inside mobile */}
-            <h1>Blog Display</h1>
-            {/*get the posts and map using keys */}
+                        <h1>Blog Display</h1>
+            {/* Get the posts and map using keys */}
             {posts.map(post => (
-                //add some styling for the posts and display them
-                <div key={post.key} style={{ marginBottom: '20px'}}>
-                    <div id='blogPost' style={{backgroundColor:'black', padding: '10px'}}>
-                        {/* display user's email to attribute to their post*/}
-                        <div id="userInfo" style={{float:'left', display:'block', marginLeft:'10%'}}>
+                // Add some styling for the posts and display them
+                <div key={post.key}>
+                    <div id='blogPost'>
+                        {/* Display user's email to attribute to their post */}
+                        <div id="userInfo">
                             <p>User: {userEmails[post.user]}</p>
                         </div>
-                        <br/>
-                        {/*display the post's body text using the html it is saved as*/}
-                        <div dangerouslySetInnerHTML={{ __html: post.body }} style={{ color: 'black', backgroundColor: 'whiteSmoke', maxWidth: '80%', display: 'block', marginLeft: '9%', marginRight: '9%', paddingLeft: '1%', paddingRight: '1%', borderRadius: '10px' }}/>
+                        {/* Display the post's body text using the HTML it is saved as */}
+                        <div dangerouslySetInnerHTML={{ __html: post.body }} />
                         {post.drawingRef && (
-                            //use the drawing functionality
+                            // Use the drawing functionality
                             <DrawingComponent drawingRef={post.drawingRef} />
                         )}
-                        {/*button to display the comment text box or hide it*/}
-                        <button 
-                            onClick={() => toggleCommentBox(post.key)} 
-                            style={{backgroundColor:'dodgerblue', marginLeft:'10%', float:'left', borderRadius:'10px', padding:'2px', paddingLeft:'5px', paddingRight:'5px'}}
-                        >Show Comments</button><br/>
+                        {/* Button to display the comment text box or hide it */}
+                        <button onClick={() => toggleCommentBox(post.key)}>Show Comments</button>
                         {openCommentBoxes[post.key] && (
-                            //use the stripped path from function to get a clean path that can be referenced to in the database  
+                            // Use the stripped path from function to get a clean path that can be referenced to in the database  
                             <CommentTextBox path={getStrippedPath(post.postRef)} />
                         )}
                     </div>
                     <div id="comments">
-                        {/* display comments*/}
+                        {/* Display comments */}
                         {openCommentBoxes[post.key] && (
                             <CommentDisplay key={`comment-${post.key}`} postId={post.key} />
                         )}
@@ -102,6 +100,8 @@ function BlogDisplay() {
                 </div>
             ))}
         </div>
+        </div>
+
     );
 }
 
