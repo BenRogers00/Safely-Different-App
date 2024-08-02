621f50460d58596853075b500e99802cd7126ed0
BenRogers00 <benr004@outlook.com>
2024-08-02 17:46:01 +1200

    new textbox editor added and old one removed

--- safely-different/src/components/blogPosts/MyEditor.jsx
+++ safely-different/src/components/blogPosts/MyEditor.jsx
@@ -3,6 +3,9 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // Import the styles
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from '../UI/HomepageComponents/NavBar';
+import WriteToDatabase from '../../databaseWriting';
+import { ref } from 'firebase/database';
+import { database } from '../../firebase/firebase';
 
 function MyEditor() {
   const [value, setValue] = useState('');
@@ -31,6 +34,13 @@ function MyEditor() {
     console.log('Editor content:', content); // This will print the content to the console for debugging
   };
 
+  // const postContent = (content) =>
+  // {
+  //   console.log("content: ", value);
+  //   const postsRef = ref(database, 'posts/');
+      
+  // }
+
   return (
     <div>
       <ReactQuill 
@@ -40,7 +50,11 @@ function MyEditor() {
         modules={modules} 
         formats={formats}
       />
+
+      {/* <button onClick={postContent}>Post </button> */}
     </div>
+
+
   );
 }
 
