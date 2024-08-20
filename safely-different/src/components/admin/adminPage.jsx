9ebbe05dd0540dc762bd95b5bf66fe9124c9dd5f
BenRogers00 <benr004@outlook.com>
2024-08-20 16:50:29 +1200

    update admin page to only be accessible by admin, even when manually going to /admin through browser

--- safely-different/src/components/admin/adminPage.jsx
+++ safely-different/src/components/admin/adminPage.jsx
@@ -1,8 +1,21 @@
-function AdminPage(){
+import AuthDetails, { AuthContext } from "../AuthDetails";
+import ReadOneDB from "../../readOneEntry";
+import { useContext } from "react";
 
+function AdminPage(){
+    const { authUser } = useContext(AuthContext); 
+
+    const isAdmin = authUser && ReadOneDB(`users/${authUser.uid}/privileges`) === "admin";
+    
+    if(isAdmin){
     return(
         <h1>ADMIN PAGE</h1>
     )
+}
+else
+return(
+    <h1>Not an admin account, access denied</h1>
+)
 }
 
 export default AdminPage
