98193cc3bd47a3e44eeaa03a35ec0f76068d3773
Andrew <wangsxin.xin@gmail.com>
2024-08-27 12:22:54 +1200

    CSS HomePage

--- safely-different/src/components/UI/HomepageComponents/Slogan.jsx
+++ safely-different/src/components/UI/HomepageComponents/Slogan.jsx
@@ -9,13 +9,13 @@ function Slogan(){
         <>
          <section className=" my-14 font-extrabold">
              <div className="text-center">
-                <p className="text-32 dark:text-purple-300 text-black">
-                First Step Solution
+                <p className="text-[45px] dark:text-teal-300 text-black">
+                First Step Solutions
                 </p>
-                <p className="text-26  text-gray-500">
+                <p className="text-[48px]  text-white">
                 Your partner for impactful delivery, custom solutions, and success. 
                 </p>
-                <button className="font-serif text-20 mt-3 py-1 px-4 font-extrabold dark:bg-purple-500 rounded-[14px] dark:hover:bg-violet-400 bg-green-100 text-black">Join us</button>
+               {/* <button className="font-serif text-20 mt-3 py-1 px-4 font-extrabold dark:bg-purple-500 rounded-[14px] dark:hover:bg-violet-400 bg-green-100 text-black">Join us</button> */}
              </div>
  
         <div>
