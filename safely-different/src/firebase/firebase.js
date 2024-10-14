8a5c93011011a5b5afe0f701020c45940025feb4
giangh00 <162215746+giangh00@users.noreply.github.com>
2024-10-15 11:02:46 +1300

    Revert "Merge branch 'main' of https://github.com/BenRogers00/Safely-Different-App"
    This reverts commit 13c894231174c06ff79fbd82992064024d9aedc4, reversing
    changes made to 4ab4661139860ec3cb79dcdcc46bd2f27f4c0b9b.

---
+++ safely-different/src/firebase/firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase, ref } from "firebase/database";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdE_nE9Nk2RHU07QVdE582mqpip59Mgx4",
+  authDomain: "safely-different.firebaseapp.com",
+  databaseURL: "https://safely-different-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "safely-different",
+  storageBucket: "safely-different.appspot.com",
+  messagingSenderId: "1039652889761",
+  appId: "1:1039652889761:web:340c3733cef4bd24343ce0",
+  measurementId: "G-ER3JKS4E2K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize services
+const auth = getAuth(app);
+const database = getDatabase(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
+
+// Export the initialized services for use in other files
+export { auth, database, storage,db };
+export default app;
